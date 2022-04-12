194b50242c5875862f8c88ce695e5670c004af00
Delegado <maxharper2013@gmail.com>
2022-04-12 01:02:09 -0300

    haciendo routing con express router

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const faker = require('faker');
-const Faker = require('faker/lib');
+const routerApi = require('./routes');
 
-// console.log("My app");
 const app = express();
 const port = 3000;
 
@@ -14,52 +12,27 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola soy una nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-  res.json(products);
-});
+routerApi(app);
 
-app.get('/products/filter', (req, res) => {
-  res.send('Yo soy un filter');
-});
+// app.get('/users', (req, res) => {
+//   const { limit, offset } = req.query;
+//   if (limit && offset) {
+//     res.json({
+//       limit,
+//       offset,
+//     });
+//   } else {
+//     res.send('No hay parametros');
+//   }
+// });
 
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params; //solo recoge le id de todos los params
-  res.json({
-    id,
-    name: 'Product 2',
-    price: 2000,
-  });
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset,
-    });
-  } else {
-    res.send('No hay parametros');
-  }
-});
-
-app.get('/categories/:categoryId/products/:prodcutId', (req, res) => {
-  const { categoryId, prodcutId } = req.params;
-  res.json({
-    categoryId,
-    prodcutId,
-  });
-});
+// app.get('/categories/:categoryId/products/:prodcutId', (req, res) => {
+//   const { categoryId, prodcutId } = req.params;
+//   res.json({
+//     categoryId,
+//     prodcutId,
+//   });
+// });
 
 app.listen(port, () => {
   console.log('mi port' + port);
