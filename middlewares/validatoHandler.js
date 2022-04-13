9e2600531ea3492198a600042993ca699937f2dd
Delegado <maxharper2013@gmail.com>
2022-04-13 04:10:18 -0300

    generamos index.html para generar solucion a errores de CORS

--- middlewares/validatoHandler.js
+++ middlewares/validatoHandler.js
@@ -2,8 +2,10 @@ const boom = require('@hapi/boom');
 
 function validatorHandler(schema, property) {
   return (req, res, next) => {
+    //creamos un middleware de forma dinamica
     const data = req[property];
-    const { error } = schema.validate(data);
+    const { error } = schema.validate(data, { abortEarly: false });
+    //abortEarly permite que Joi junte todos los errores exitentes y los envie
     if (error) {
       next(boom.badRequest(error));
     }
--- schemas/productSchemas.js
+++ schemas/productSchemas.js
@@ -1,17 +1,20 @@
 const Joi = require('joi'); //libreria que valida nuestros datos
 
-const id = Joi.string().uuid; //tipo strig y de tipo id
-const name = Joi.string().alphanum().min(3).max(15); //tipo string alfa numerico de 3 a 15 caracteres
+const id = Joi.string().uuid(); //tipo strig y de tipo id
+const name = Joi.string().min(3).max(15); //tipo string alfa numerico de 3 a 15 caracteres
 const price = Joi.number().integer().min(10);
+const image = Joi.string().uri();
 
 const createProdectSchema = Joi.object({
   name: name.required(), //el nombre es requerido para la creacion de nuevo prod
   price: price.required(),
+  image: image.required(),
 });
 
 const updateProductSchema = Joi.object({
   name: name, //para el update usamos la validacion declarada al comienzo para name
   price: price, //lo mismo para price pero ninguno de los dos es requerido si o si
+  image: image,
 });
 
 const getProductSchema = Joi.object({
