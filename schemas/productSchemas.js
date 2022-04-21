dfba65bcebfeb3906b76023b9bffb5ae83223414
Delegado <maxharper2013@gmail.com>
2022-04-21 02:44:32 -0300

    agregando tablas products y categories con relaciones 1 a n

--- schemas/productSchemas.js
+++ schemas/productSchemas.js
@@ -3,11 +3,14 @@ const Joi = require('joi'); //libreria que valida nuestros datos
 const id = Joi.string().uuid(); //tipo strig y de tipo id
 const name = Joi.string().min(3).max(15); //tipo string alfa numerico de 3 a 15 caracteres
 const price = Joi.number().integer().min(10);
+const description = Joi.string().min(10);
+
 const image = Joi.string().uri();
 
 const createProdectSchema = Joi.object({
   name: name.required(), //el nombre es requerido para la creacion de nuevo prod
   price: price.required(),
+  description: description.required(),
   image: image.required(),
 });
 
@@ -15,6 +18,7 @@ const updateProductSchema = Joi.object({
   name: name, //para el update usamos la validacion declarada al comienzo para name
   price: price, //lo mismo para price pero ninguno de los dos es requerido si o si
   image: image,
+  description
 });
 
 const getProductSchema = Joi.object({
